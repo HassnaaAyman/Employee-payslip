244ce69262b1c2c39cafe08ae1e3496a5d5aaae5
Hasnaa Ayman <hassnaaayman16@gmail.com>
2021-10-20 21:20:47 +0200

    add styled components and its babel configrations, antd for the employees form

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { EmployeeProvider } from "../context/state";
+import "antd/dist/antd.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
